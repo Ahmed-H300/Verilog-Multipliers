a9bef0f79e25f8574afa37e90ca30f2c06edb98a
MoazHassan2022 <87096647+MoazHassan2022@users.noreply.github.com>
2022-12-18 00:26:48 +0200

    Finsihing floating point multiplier

--- floatingMultiplier/floatingMultiplierTB.v
+++ floatingMultiplier/floatingMultiplierTB.v
@@ -1,9 +1,9 @@
-module MULXTB;
-reg signed [31:0] a, b;
-wire signed [63:0] MulXResult;
-wire overflowMulX;
+module floatingMultiplierTB;
+reg [31:0] a, b;
+wire [31:0] floatingPointResult;
+wire overflowFloatingPoint, exceptionFloatingPoint;
 reg clk, reset, en;
-MulXWithRegs MulXWithRegsInst(a, b, clk, reset, en, MulXResult, overflowMulX);
+floatingMultiplierWithRegs floatingMultiplierWithRegsInst(a, b, clk, reset, en, floatingPointResult, overflowFloatingPoint, exceptionFloatingPoint);
 integer passed, failed;
 initial begin
 	clk = 1;
@@ -14,93 +14,93 @@ initial begin
 	#50;
 	#50;
 	reset = 0;
-	a = 5;
-    b = -7;
+	a = 32'b01000000101001000000000000000000; // 5.125
+    b = 32'b11000000111100000000000000000000; // -7.5
 	#50;
 	#50;
-	if (MulXResult === -35) begin  
+	if (floatingPointResult === 32'b11000010000110011100000000000000) begin  // -38.4375
 		passed = passed + 1;
 		$display("TestCase#1: success");
 	end else begin
 		failed = failed +1;
-		$display("TestCase#1: failed with Input %d, %d, Output MulXResult: %d, Overflow X multiplier: %d", a, b, MulXResult, overflowMulX);
+		$display("TestCase#1: failed with Input %f, %f, Output floatingPointResult: %f, Overflow Floating Multiplier: %b, Exception Floating Multiplier: %b", a, b, floatingPointResult, overflowFloatingPoint, exceptionFloatingPoint);
 	end
-	a = 2;
-    b = 3;
+	a = 32'b01000000000000000000000000000000; // 2.0
+    b = 32'b01000000011100000000000000000000; // 3.75
 	#50;
 	#50;
-	if (MulXResult === 6) begin  
+	if (floatingPointResult === 32'b01000000111100000000000000000000) begin  // 7.5
 		passed = passed + 1;
 		$display("TestCase#2: success");
 	end else begin
 		failed = failed +1;
-		$display("TestCase#2: failed with Input %d, %d, Output MulXResult: %d, Overflow X multiplier: %d", a, b, MulXResult, overflowMulX);
+		$display("TestCase#2: failed with Input %f, %f, Output floatingPointResult: %f, Overflow Floating Multiplier: %b, Exception Floating Multiplier: %b", a, b, floatingPointResult, overflowFloatingPoint, exceptionFloatingPoint);
 	end
-	a = -12;
-    b = -4;
+	a = 32'b11000001010001000000000000000000; // -12.25
+    b = 32'b11000000100100000000000000000000; // -4.5
 	#50;
 	#50;
-	if (MulXResult === 48) begin 
+	if (floatingPointResult === 32'b01000010010111001000000000000000) begin  // 55.125
 		passed = passed + 1; 
 		$display("TestCase#3: success");
 	end else begin
 		failed = failed +1;
-		$display("TestCase#3: failed with Input %d, %d, Output MulXResult: %d, Overflow X multiplier: %d", a, b, MulXResult, overflowMulX);
+		$display("TestCase#3: failed with Input %f, %f, Output floatingPointResult: %f, Overflow Floating Multiplier: %b, Exception Floating Multiplier: %b", a, b, floatingPointResult, overflowFloatingPoint, exceptionFloatingPoint);
 	end
-	a = -9;
-    b = 5;
+	a = 32'b11000001000100000000000000000000; // -9.0
+    b = 32'b01000000101000100000000000000000; // 5.0625
 	#50;
 	#50;
-	if (MulXResult === -45) begin  
+	if (floatingPointResult === 32'b11000010001101100100000000000000) begin  // -45.5625
 		passed = passed + 1;
 		$display("TestCase#4: success");
 	end else begin
 		failed = failed +1;
-		$display("TestCase#4: failed with Input %d, %d, Output MulXResult: %d, Overflow X multiplier: %d", a, b, MulXResult, overflowMulX);
+		$display("TestCase#4: failed with Input %f, %f, Output floatingPointResult: %f, Overflow Floating Multiplier: %b, Exception Floating Multiplier: %b", a, b, floatingPointResult, overflowFloatingPoint, exceptionFloatingPoint);
 	end
-	a = 11;
+	a = 32'b00111101000000000000000000000000; // 0.03125
     b = 0;
 	#50;
 	#50;
-	if (MulXResult === 0) begin  
+	if (floatingPointResult === 0) begin  
 		passed = passed + 1;
 		$display("TestCase#5: success");
 	end else begin
 		failed = failed +1;
-		$display("TestCase#5: failed with Input %d, %d, Output MulXResult: %d, Overflow X multiplier: %d", a, b, MulXResult, overflowMulX);
+		$display("TestCase#5: failed with Input %f, %f, Output floatingPointResult: %f, Overflow Floating Multiplier: %b, Exception Floating Multiplier: %b", a, b, floatingPointResult, overflowFloatingPoint, exceptionFloatingPoint);
 	end
-	a = 10;
-    b = 1;
+	a = 32'b00111101000000000000000000000000; // 0.03125
+    b = 32'b00111111100000000000000000000000;
 	#50;
 	#50;
-	if (MulXResult === 10) begin  
+	if (floatingPointResult === 32'b00111101000000000000000000000000) begin  // 0.03125
 		passed = passed + 1;
 		$display("TestCase#6: success");
 	end else begin
 		failed = failed +1;
-		$display("TestCase#6: failed with Input %d, %d, Output MulXResult: %d, Overflow X multiplier: %d", a, b, MulXResult, overflowMulX);
+		$display("TestCase#6: failed with Input %f, %f, Output floatingPointResult: %f, Overflow Floating Multiplier: %b, Exception Floating Multiplier: %b", a, b, floatingPointResult, overflowFloatingPoint, exceptionFloatingPoint);
 	end
-	a = 4;
-    b = 6;
+	a = 32'b00111100100000000000000000000000; // 0.015625
+    b = 32'b01000010110010000000000000000000; // 100
 	#50;
 	#50;
-	if (MulXResult === 24) begin  
+	if (floatingPointResult === 32'b00111111110010000000000000000000) begin  // 1.5625
 		passed = passed + 1;
 		$display("TestCase#7: success");
 	end else begin
 		failed = failed +1;
-		$display("TestCase#7: failed with Input %d, %d, Output MulXResult: %d, Overflow X multiplier: %d", a, b, MulXResult, overflowMulX);
+		$display("TestCase#7: failed with Input %f, %f, Output floatingPointResult: %f, Overflow Floating Multiplier: %b, Exception Floating Multiplier: %b", a, b, floatingPointResult, overflowFloatingPoint, exceptionFloatingPoint);
 	end
-	a = -1;
-    b = -7;
+	a = 32'b10111111100000000000000000000000; // -1
+    b = 32'b11000000111000000000000000000000; // -7
 	#50;
 	#50;
-	if (MulXResult === 7) begin  
+	if (floatingPointResult === 32'b01000000111000000000000000000000) begin  // 7 
 		passed = passed + 1;
 		$display("TestCase#8: success");
 	end else begin
 		failed = failed +1;
-		$display("TestCase#8: failed with Input %d, %d, Output MulXResult: %d, Overflow X multiplier: %d", a, b, MulXResult, overflowMulX);
+		$display("TestCase#8: failed with Input %f, %f, Output floatingPointResult: %f, Overflow Floating Multiplier: %b, Exception Floating Multiplier: %b", a, b, floatingPointResult, overflowFloatingPoint, exceptionFloatingPoint);
 	end
 	$display("Total passed tests: %d and Total failed tests: %d", passed, failed);
 end
--- floatingMultiplier/floatingMultiplierWithRegs.v
+++ floatingMultiplier/floatingMultiplierWithRegs.v
@@ -1,17 +1,16 @@
-module MulXWithRegs #(parameter N = 32) (inputA, inputB, clk, reset, en, result, overflow);
+module floatingMultiplierWithRegs #(parameter N = 32) (inputA, inputB, clk, reset, en, result, overflow, exception);
 input clk, reset, en;
 input [N-1:0] inputA, inputB;
-output overflow;
-output [2*N-1:0] result;
+output overflow, exception;
+output [N-1:0] result;
 wire [N-1:0] A_reg;
 wire [N-1:0] B_reg;
-wire [N-1:0] outA_reg;
-wire [N-1:0] outB_reg;
-wire overflow_reg;
+wire [N-1:0] out_reg;
+wire overflow_reg, exception_reg;
 registerNbits #(32) regA(clk, reset, en, inputA, A_reg);
 registerNbits #(32) regB(clk, reset, en, inputB, B_reg);
-XMult XMultInst(A_reg, B_reg, {outA_reg,outB_reg}, overflow_reg);
-registerNbits #(32) outA(clk, reset, en, outB_reg, result[N-1:0]);
-registerNbits #(32) outB(clk, reset, en, outA_reg, result[2*N-1:N]);
+floatingMultiplier floatingMultiplierInst(A_reg, B_reg, out_reg, overflow_reg, exception_reg);
+registerNbits #(32) outReg(clk, reset, en, out_reg, result);
 registerNbits #(1) outOverflow(clk, reset, en, overflow_reg, overflow);
+registerNbits #(1) outException(clk, reset, en, exception_reg, exception);
 endmodule
