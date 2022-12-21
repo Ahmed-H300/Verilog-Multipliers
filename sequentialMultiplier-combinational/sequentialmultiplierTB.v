a645b4a7e7fb8b4ec783851952a24703368a9970
Ahmed-H300 <ahmed300hany@gmail.com>
2022-12-21 23:09:52 +0200

    sequential multiplier

--- sequentialMultiplier-combinational/sequentialmultiplierTB.v
+++ sequentialMultiplier-combinational/sequentialmultiplierTB.v
@@ -1,112 +1,110 @@
 module sequentialmultiplierTB;
-reg [31:0] a, b;
+reg signed [31:0] a, b;
 wire signed [63:0] sequentialmultiplierResult;
 reg clk, reset, en;
 sequentialmultiplier_withregs SMTB(a, b, clk, reset, en, sequentialmultiplierResult);
 integer passed, failed;
 initial begin
-	// Clock cycle time = 20
 	passed = 0;
 	failed = 0;
-	clk = 0;
+	clk = 1;
+	reset = 1;
 	en = 1;
+	#50;
+	#50;
+	reset = 0;
 	a = 5;
     b = -7;
-	reset = 1;
-	#20;
-	reset = 0;
-	#20;
-	#640;
-	a = 2;
-    b = 3;
-	#20;
+	#50;
+	#50;
 	if (sequentialmultiplierResult === -35) begin  
 		passed = passed + 1;
 		$display("TestCase#1: success");
 	end else begin
 		failed = failed +1;
-		$display("TestCase#1: failed with Input %d, %d, Output sequentialmultiplier: %d", a, b, sequentialmultiplierResult);
+		$display("TestCase#1: failed with Input %d, %d, Output sequentialmultiplierResult: %d", a, b, sequentialmultiplierResult);
 	end
-	#640;
-	a = -12;
-    b = -4;
-	#20;
+	a = 2;
+    b = 3;
+	#50;
+	#50;
 	if (sequentialmultiplierResult === 6) begin  
 		passed = passed + 1;
 		$display("TestCase#2: success");
 	end else begin
 		failed = failed +1;
-		$display("TestCase#2: failed with Input %d, %d, Output sequentialmultiplier: %d", a, b, sequentialmultiplierResult);
+		$display("TestCase#2: failed with Input %d, %d, Output sequentialmultiplierResult: %d", a, b, sequentialmultiplierResult);
 	end
-	#640;
-	a = -9;
-    b = 5;
-	#20;
+	a = -12;
+    b = -4;
+	#50;
+	#50;
 	if (sequentialmultiplierResult === 48) begin 
 		passed = passed + 1; 
 		$display("TestCase#3: success");
 	end else begin
 		failed = failed +1;
-		$display("TestCase#3: failed with Input %d, %d, Output sequentialmultiplier: %d", a, b, sequentialmultiplierResult);
+		$display("TestCase#3: failed with Input %d, %d, Output sequentialmultiplierResult: %d", a, b, sequentialmultiplierResult);
 	end
-	#640;
-	a = 11;
-    b = 0;
-	#20;
+	a = -9;
+    b = 5;
+	#50;
+	#50;
 	if (sequentialmultiplierResult === -45) begin  
 		passed = passed + 1;
 		$display("TestCase#4: success");
 	end else begin
 		failed = failed +1;
-		$display("TestCase#4: failed with Input %d, %d, Output sequentialmultiplier: %d", a, b, sequentialmultiplierResult);
+		$display("TestCase#4: failed with Input %d, %d, Output sequentialmultiplierResult: %d", a, b, sequentialmultiplierResult);
 	end
-	#640;
-	a = 10;
-    b = 1;
-	#20;
+	a = 11;
+    b = 0;
+	#50;
+	#50;
 	if (sequentialmultiplierResult === 0) begin  
 		passed = passed + 1;
 		$display("TestCase#5: success");
 	end else begin
 		failed = failed +1;
-		$display("TestCase#5: failed with Input %d, %d, Output sequentialmultiplier: %d", a, b, sequentialmultiplierResult);
+		$display("TestCase#5: failed with Input %d, %d, Output sequentialmultiplierResult: %d", a, b, sequentialmultiplierResult);
 	end
-	#640;
-	a = 4;
-    b = 6;
-	#20;
+	a = 10;
+    b = 1;
+	#50;
+	#50;
 	if (sequentialmultiplierResult === 10) begin  
 		passed = passed + 1;
 		$display("TestCase#6: success");
 	end else begin
 		failed = failed +1;
-		$display("TestCase#6: failed with Input %d, %d, Output sequentialmultiplier: %d", a, b, sequentialmultiplierResult);
+		$display("TestCase#6: failed with Input %d, %d, Output sequentialmultiplierResult: %d", a, b, sequentialmultiplierResult);
 	end
-	#640;
-	a = -1;
-    b = -7;
-	#20;
+	a = 4;
+    b = 6;
+	#50;
+	#50;
 	if (sequentialmultiplierResult === 24) begin  
 		passed = passed + 1;
 		$display("TestCase#7: success");
 	end else begin
 		failed = failed +1;
-		$display("TestCase#7: failed with Input %d, %d, Output sequentialmultiplier: %d", a, b, sequentialmultiplierResult);
+		$display("TestCase#7: failed with Input %d, %d, Output sequentialmultiplierResult: %d", a, b, sequentialmultiplierResult);
 	end
-	#640;
-	#20;
+	a = -1;
+    b = -7;
+	#50;
+	#50;
 	if (sequentialmultiplierResult === 7) begin  
 		passed = passed + 1;
 		$display("TestCase#8: success");
 	end else begin
 		failed = failed +1;
-		$display("TestCase#8: failed with Input %d, %d, Output sequentialmultiplier: %d", a, b, sequentialmultiplierResult);
+		$display("TestCase#8: failed with Input %d, %d, Output sequentialmultiplierResult: %d", a, b, sequentialmultiplierResult);
 	end
 	$display("Total passed tests: %d and Total failed tests: %d", passed, failed);
 end
 always begin
-	#10;
+	#25;
 	clk = ~clk;
 end
 endmodule
-
--- sequentialMultiplier/sequentialmultiplier.v
+++ sequentialMultiplier/sequentialmultiplier.v
@@ -1,32 +1,63 @@
-module sequentialmultiplier (input [31:0] in1, input [31:0] in2, output [63:0] result);
-
-integer i;
+module sequentialmultiplier (input [31:0] in1, input [31:0] in2, input clk, input reset, input en, output reg [63:0] result, output reg enableOutput);
 
 wire [31:0] m;
 wire [31:0] q;
-wire [63:0] out;
+reg resetReg;
+reg [5:0] counter;
 
-assign m = (in1[31] == 1) ? (-in1): in1;
-assign q = (in2[31] == 1) ? (-in2): in2;
+assign m = (in1[31] == 1) ? (~in1 + 1'b1): in1;
+assign q = (in2[31] == 1) ? (~in2 + 1'b1): in2;
 reg [64:0]res=0;
-assign result = out;
-assign out = ((in1[31] ^ in2[31]) == 1) ? (-res[63:0]) : res[63:0];
 
-always @(m or q) begin
+always @(posedge clk) begin
+if (en === 1'b1) begin
+enableOutput = 0;
+if (reset === 1'b1) begin
+counter = 0;
+result = 0;
+resetReg = 1;
+end
+else if (resetReg === 1'b1) begin
+counter = 0;
+result = 0;
+resetReg = 0; // make it waits 1 cycle after reset is gone to 0 to get right data
+end
+else if (counter === 0)begin
 res = {33'b0,q};
-for(i = 0; i < 32; i = i + 1) begin
-
-if(res[0] == 0)begin
-res = res >> 1;
+if(res[0] === 1'b1)begin
+res[64:32] = res[63:32] + m;
+//$display("res = %d",res);
+end
+res = {1'b0,res[64:1]};
+counter = 1;
 end
 else begin
+if(res[0] === 1'b1)begin
 res[64:32] = res[63:32] + m;
-res = res >> 1;
+//$display("res = %d",res);
+end
+res = {1'b0,res[64:1]};
+counter = counter + 1;
+if (counter === 6'd33) begin
+counter = 0;
+end
+if (counter === 6'd32) begin 
+res = ((in1[31] ^ in2[31]) == 1) ? (~res[63:0] + 1'b1) : res[63:0];
+result = res; // last iteration
+// make one cycle delay for getting input data right
+enableOutput = 1; // write the previos result out to the out register
+end
+end
+
 end
 
 
 
-end
+
+
+
 
 end
 endmodule
+
+
