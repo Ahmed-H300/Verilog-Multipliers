2fa59afd52008882746d49adc2f568c5b6b8de3d
Ahmed-H300 <ahmed300hany@gmail.com>
2023-01-04 11:39:42 +0200

    adding test benches

--- boothMultiplier/boothmultiplierTB.v
+++ boothMultiplier/boothmultiplierTB.v
@@ -10,17 +10,17 @@ initial begin
 	failed = 0;
 	clk = 0;
 	en = 1;
-	a = 5;
-    b = -7;
+	a = 553524;
+    b = 840;
 	reset = 1;
 	#20;
 	reset = 0;
 	#20;
 	#640;
-	a = 2;
-    b = 3;
+	a = 32'h50647236;
+    b = 32'h50612336;
 	#20;
-	if (boothmultiplierResult === -35) begin  
+	if (boothmultiplierResult === 64'd464960160) begin  
 		passed = passed + 1;
 		$display("TestCase#1: success");
 	end else begin
@@ -28,10 +28,10 @@ initial begin
 		$display("TestCase#1: failed with Input %d, %d, Output boothmultiplier: %d", a, b, boothmultiplierResult);
 	end
 	#640;
-	a = -12;
-    b = -4;
+	a = 553524;
+    b = -259;
 	#20;
-	if (boothmultiplierResult === 6) begin  
+	if (boothmultiplierResult === 64'h193DE4CED7437964) begin  
 		passed = passed + 1;
 		$display("TestCase#2: success");
 	end else begin
@@ -39,10 +39,10 @@ initial begin
 		$display("TestCase#2: failed with Input %d, %d, Output boothmultiplier: %d", a, b, boothmultiplierResult);
 	end
 	#640;
-	a = -9;
-    b = 5;
+	a = 32'h50647236;
+    b = 32'hB887CAAF;
 	#20;
-	if (boothmultiplierResult === 48) begin 
+	if (boothmultiplierResult === -143362716) begin 
 		passed = passed + 1; 
 		$display("TestCase#3: success");
 	end else begin
@@ -50,10 +50,10 @@ initial begin
 		$display("TestCase#3: failed with Input %d, %d, Output boothmultiplier: %d", a, b, boothmultiplierResult);
 	end
 	#640;
-	a = 11;
-    b = 0;
+	a = -259;
+    b = 553524;
 	#20;
-	if (boothmultiplierResult === -45) begin  
+	if (boothmultiplierResult === 64'hE98E647F4142AEEA) begin  
 		passed = passed + 1;
 		$display("TestCase#4: success");
 	end else begin
@@ -61,10 +61,10 @@ initial begin
 		$display("TestCase#4: failed with Input %d, %d, Output boothmultiplier: %d", a, b, boothmultiplierResult);
 	end
 	#640;
-	a = 10;
-    b = 1;
+	a = -1199060305;
+    b = 1348760118;
 	#20;
-	if (boothmultiplierResult === 0) begin  
+	if (boothmultiplierResult === -143362716) begin  
 		passed = passed + 1;
 		$display("TestCase#5: success");
 	end else begin
@@ -72,10 +72,10 @@ initial begin
 		$display("TestCase#5: failed with Input %d, %d, Output boothmultiplier: %d", a, b, boothmultiplierResult);
 	end
 	#640;
-	a = 4;
-    b = 6;
+	a = -259;
+    b = -259;
 	#20;
-	if (boothmultiplierResult === 10) begin  
+	if (boothmultiplierResult === -(64'd1617244718460915990)) begin  
 		passed = passed + 1;
 		$display("TestCase#6: success");
 	end else begin
@@ -83,10 +83,10 @@ initial begin
 		$display("TestCase#6: failed with Input %d, %d, Output boothmultiplier: %d", a, b, boothmultiplierResult);
 	end
 	#640;
-	a = -1;
-    b = -7;
+	a = 32'hB887CAAF;
+    b = 32'h887CAAF3;
 	#20;
-	if (boothmultiplierResult === 24) begin  
+	if (boothmultiplierResult === 67081) begin  
 		passed = passed + 1;
 		$display("TestCase#7: success");
 	end else begin
@@ -94,13 +94,58 @@ initial begin
 		$display("TestCase#7: failed with Input %d, %d, Output boothmultiplier: %d", a, b, boothmultiplierResult);
 	end
 	#640;
+	a = 1;
+    	b = 1348760118;
 	#20;
-	if (boothmultiplierResult === 7) begin  
+	if (boothmultiplierResult === 64'h215D8B0A7A419A1D) begin  
 		passed = passed + 1;
 		$display("TestCase#8: success");
 	end else begin
 		failed = failed +1;
 		$display("TestCase#8: failed with Input %d, %d, Output boothmultiplier: %d", a, b, boothmultiplierResult);
+	end
+	//$display("Total passed tests: %d and Total failed tests: %d", passed, failed);
+	#640;
+	a = -1199060305;
+    	b = 1;
+	#20;
+	if (boothmultiplierResult === 1348760118) begin  
+		passed = passed + 1;
+		$display("TestCase#9: success");
+	end else begin
+		failed = failed +1;
+		$display("TestCase#9: failed with Input %h, %h, Output boothmultiplier: %h", a, b, boothmultiplierResult);
+	end
+	#640;
+	a = 0;
+    b = 1348760118;
+	#20;
+	if (boothmultiplierResult === -1199060305) begin  
+		passed = passed + 1;
+		$display("TestCase#10: success");
+	end else begin
+		failed = failed +1;
+		$display("TestCase#10: failed with Input %h, %h, Output boothmultiplier: %h", a, b, boothmultiplierResult);
+	end
+	#640;
+	a = -1199060305;
+    b = 0;
+	#20;
+	if (boothmultiplierResult === 0) begin  
+		passed = passed + 1;
+		$display("TestCase#11: success");
+	end else begin
+		failed = failed +1;
+		$display("TestCase#11: failed with Input %h, %h, Output boothmultiplier: %h", a, b, boothmultiplierResult);
+	end
+	#640;
+	#20;
+	if (boothmultiplierResult === 0) begin  
+		passed = passed + 1;
+		$display("TestCase#12: success");
+	end else begin
+		failed = failed +1;
+		$display("TestCase#12: failed with Input %h, %h, Output boothmultiplier: %h", a, b, boothmultiplierResult);
 	end
 	$display("Total passed tests: %d and Total failed tests: %d", passed, failed);
 end
